1ae3087273c81ae4f298c9ad7111d3cb4a85b731
Steven McCombe <stevenmccombe93@gmail.com>
2023-07-06 00:39:12 -0400

    got app deployed. Error finding the pages.

---
+++ server.js
@@ -0,0 +1,22 @@
+const { https } = require('firebase-functions')
+const { default: next } = require('next')
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const server = next({
+    dev: isDev,
+    conf: { distDir: '.next' }
+})
+
+const nextjsHandle = server.getRequestHandler();
+//we will create our firebase function
+
+exports.nextServer2 = https.onRequest((req, res) => {
+    try {
+        return server.prepare().then(() => {
+            return nextjsHandle(req, res);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
